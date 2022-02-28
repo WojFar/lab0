7b6aa8e81b0177d5d8da34ed196b408b3c52d480
wojciech.farej <228730@edu.p.lodz.pl>
2022-02-28 17:53:15 +0100

    working tails method

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -4,9 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TailsGenerator {
-
+    
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> list = new ArrayList<>();
+        list.add(value);
+        list.add("nieiskotne");
+        list.add(value.substring(2));
+        for(int i =0;i<value.length() - 3;i++)
+            list.add("nieiskotne");
+        list.add("");
+        
+        return list;
     }
 
 }
